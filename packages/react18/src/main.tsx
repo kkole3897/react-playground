27773f65c321aba73fe14f8baa6d99d4c0b75a6a
Jin Kwan Kim <kkole3897@gmail.com>
2024-01-03 00:01:41 +0900

    chore: react-query 추가

--- packages/react18/src/main.tsx
+++ packages/react18/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import SuspsnePage from "./pages/examples/Suspense.tsx";
 
 const router = createBrowserRouter([
@@ -34,10 +35,14 @@ async function enableMocking() {
   return worker.start();
 }
 
+const queryClient = new QueryClient();
+
 enableMocking().then(() => {
   ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </React.StrictMode>,
   );
 });
